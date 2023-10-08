8ac965fad6f04eed684537646678e6aa197cc3af
Ru28 <rupeshvirani01@gmail.com>
2023-10-08 11:26:26 +0530

    day-1

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../Todo"
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit{
+ todos:Todo[]=[];
+ constructor(){
+    this.todos=[
+      {
+        sno:1,
+        title: "this is title1",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno:2,
+        title: "this is title2",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno:3,
+        title: "this is title3",
+        desc: "Description",
+        active: true
+      }
+   ]
+  }
+ ngOnInit(): void{
+  
+ }
+ deleteTodo(todo:Todo){
+  console.log(todo);
+  const index=this.todos.indexOf(todo);
+  this.todos.splice(index,1)
+ }
+
+}
